3edd48cd3c7f1ff0b40a5abf4b260f58959fc3e9
carloszamo077 <carloszamo077@gmail.com>
2018-04-24 14:46:26 -0600

    Initial commit

---
+++ platforms/android/assets/www/plugins/cordova-plugin-mfp/worklight/checksum.js
@@ -0,0 +1,1 @@
+var WL_CHECKSUM = {"checksum":0,"date":1524602370614,"machine":"L540-CZRAMIREZ"}
---
+++ platforms/android/assets/www/plugins/cordova-plugin-mfp/worklight/static_app_props.js
@@ -0,0 +1,16 @@
+// This is a generated file. Do not edit. See application-descriptor.xml.
+// WLClient configuration variables.
+console.log("Running static_app_props.js...");
+var WL = WL ? WL : {};
+WL.StaticAppProps = { APP_ID: 'com.gbm.mobilefirst.example',
+  APP_VERSION: '1.0.0',
+  WORKLIGHT_PLATFORM_VERSION: '8.0.0.00-20180405-080909',
+  WORKLIGHT_NATIVE_VERSION: '3308029348',
+  LANGUAGE_PREFERENCES: 'en',
+  ENVIRONMENT: 'android',
+  WORKLIGHT_ROOT_URL: '/apps/services/api/com.gbm.mobilefirst.example/android/',
+  APP_SERVICES_URL: '/apps/services/',
+  APP_DISPLAY_NAME: 'MobileFirstGBM',
+  LOGIN_DISPLAY_TYPE: 'embedded',
+  mfpClientCustomInit: false,
+  MESSAGES_DIR: 'plugins\\cordova-plugin-mfp\\worklight\\messages' };
